a287b776a768f8d1def652a1fe370773511c4616
Yeison Luna <yeisonluna99@hotmail.com>
2023-03-08 15:26:39 -0500

    uso de pandas para lectura de archivos csv

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
   
 
 def run():
+  '''
   continente = input('Type continent ==>  ')
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda i: i['Continent'] == continente, data))
@@ -11,8 +13,23 @@ def run():
   countries = list(map(lambda x : x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages, titulo)
+  '''
+  titulo = 'Diagrama de Población de Paises'
+
+  df = pd.read_csv('data.csv') #Leo el archivo con pandas
+  df = df[df['Continent']=='Europe'] #Filtro la columna continet con South America
   
+
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+
+  charts.generate_pie_chart(countries, percentages, titulo)
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type country ==> ')
+  print(country)
+
+
   result = utils.population_by_country(data, country)
 
   if len(result)>0:
